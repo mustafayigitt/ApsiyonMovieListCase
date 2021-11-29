9b5bb6944905011613efa882ae814a3a12a3f003
Mustafa YIGIT <mustafa.yt65@gmail.com>
2021-11-29 17:16:07 +0300

    add call timeout to requests

--- app/src/main/java/com/mustafayigit/apsiyonmovielistcase/di/NetworkModule.kt
+++ app/src/main/java/com/mustafayigit/apsiyonmovielistcase/di/NetworkModule.kt
@@ -13,6 +13,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -29,6 +30,7 @@ object NetworkModule {
     @Singleton
     fun provideOkhttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
+            .callTimeout(30,TimeUnit.SECONDS)
             .addInterceptor(HttpLoggingInterceptor().also {
                 it.setLevel(HttpLoggingInterceptor.Level.BODY)
             })
